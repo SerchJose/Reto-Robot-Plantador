fc632f7dd5d24a34f8e16288e524455a5624932e
Jose Luis <jose.alvarez@beeckerco.com>
2024-04-09 18:32:54 -0600

    util modificado a rojo y main con modificacion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import cv2
 from PIL import Image
-from util import get_limits
+from util1 import get_limits
 
 
 yellow = [0, 255, 255]  # yellow in BGR colorspace
@@ -24,6 +24,7 @@ while True:
         frame = cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
 
     cv2.imshow('frame', frame)
+    cv2.imshow('mask', mask)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
