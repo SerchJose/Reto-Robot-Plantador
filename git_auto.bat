3582f1566d7c707c5b965f30bb25f5d2b78e1d0c
SerchJose <a01367575@tec.mx>
2024-04-22 18:19:10 -0600

    GIT AUTO

--- git_auto.bat
+++ git_auto.bat
@@ -1,12 +1,16 @@
 @echo off
+
 REM Agregar todos los archivos al staging area
 git add .
 
-REM Hacer el commit con un mensaje predeterminado
-git commit -m "Auto commit"
+REM Solicitar al usuario que ingrese el mensaje de commit
+set /p commitMessage="Ingrese el mensaje de commit: "
+
+REM Hacer el commit con el mensaje personalizado
+git commit -m "%commitMessage%"
 
 REM Hacer el push al repositorio remoto
-git push origin Suspension
+git push
 
 REM Pausa para ver los resultados
 pause
