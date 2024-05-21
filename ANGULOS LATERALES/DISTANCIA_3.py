e7ebb03570c445cd80ae3b1f9a779c60db3e9c37
SerchJose <a01367575@tec.mx>
2024-05-21 17:58:10 -0600

    SS

--- ANGULOS LATERALES/DISTANCIA_3.py
+++ ANGULOS LATERALES/DISTANCIA_3.py
@@ -1,10 +1,11 @@
 import time
-import board
 import busio
 import adafruit_vl53l0x
 
-# Initialize I2C bus and sensor
-i2c = busio.I2C(board.SCL, board.SDA)
+# Initialize I2C bus
+i2c = busio.I2C(3, 2)  # Use the correct I2C bus number for your setup (3 and 2 are just examples)
+
+# Initialize the VL53L0X sensor
 vl53 = adafruit_vl53l0x.VL53L0X(i2c)
 
 # Optionally adjust the measurement timing budget (in ms)
